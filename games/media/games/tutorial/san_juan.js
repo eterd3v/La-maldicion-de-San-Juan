7e4e346fe9932cfe9c3d4c1efc5988438941edfe
Antonio Javier Armenteros Martínez <ajam0011@red.ujaen.es>
2022-03-15 22:01:54 +0100

    Salir de casa terminado

--- games/media/games/tutorial/san_juan.js
+++ games/media/games/tutorial/san_juan.js
@@ -27,7 +27,187 @@ undum.game.start = "comienzo";
 /* The situations that the game can be in. Each has a unique ID. */
 undum.game.situations = {
     comienzo: new undum.SimpleSituation(
-        "<h1>Comienzo</h1>" +
+        "<h1>En el balcón de mi casa</h1>" +
+        "<p>Con un cielo despejado y junto a esa temperatura que sólo tiene el verano " +
+        "te encuentras en el balcón de tu casa mirando el cielo como quién admira un paisaje. Todo está tranquilo en tu pueblo.</p>" +
+        "</br></br>" + "<p class='transient'>De repente te llaman al teléfono móvil, así que contestas..</p>" +
+        "<p class='transient'><em><a href='llamada1'>¿Diga?</a></em></br></p>" +
+        "<p class='transient'><em><a href='llamada1'>¡Hola!</a></em></br></p>" +
+        "<p class='transient'><em><a href='llamada1'>Hola ¿quién es?</a></em></br></p>"
+        , {}
+    ),
+    llamada1: new undum.SimpleSituation(
+        "<p class='transient'></br><em>????? - ¡Hey! ¿Vas a venir hoy a ver los fuegos al final? </em>." +
+        "</br> Te percatas de que es tu amigo Pedro así que respondes:</br>" +
+        "<em>¿Estará también...</em>" +
+        "<p class='transient'><em><a href='llamada2'>Manolo?.</a></em></br></p>" +
+        "<p class='transient'><em><a href='llamada2'>Lucía?.</a></em></br></p>" +
+        "<p class='transient'><em><a href='llamada2'>Jose?.</a></em></br></p>" +
+        "<p class='transient'> <em><a href='llamada2'>María?.</a></em></br></p>"
+        , {
+            actions: {
+                "op1": "<p><em>Yo - ¿Estará también Manolo?</em></p>",
+                "op2": "<p><em>Yo - ¿Estará también Lucía?</em></p>",
+                "op3": "<p><em>Yo - ¿Estará también Jose?</em></p>",
+                "op4": "<p><em>Yo - ¿Estará también María?</em></p>"
+            }
+        }
+    ),
+    llamada2: new undum.SimpleSituation(
+        "<p class='transient'></br>Pedro - <em>Sí, también viene. Bueno, ¿qué me dices? que llevo dos días detras de tí jajaja</em></p>" +
+        "<p class='transient'> <em><a href='llamada3'>Bueeeeeeeeeeeeno, me apunto.</a></em>" +
+        "<p class='transient'> <a href='./opc2' class='once'><em>No tengo muchas ganas hoy la verdad....</em></a></p>" +
+        "<p class='transient'> <a href='./opc3' class='once'><em>Es que tengo que....</em></a></p>"
+        , {
+            actions: {
+                "opc2": "<br><p class='transient'><em>Yo - No tengo muchas ganas hoy la verdad....</em></p>" +
+                    "<p class='transient'><em>Pedro - No te creo. Con lo que tu eras... Además hay comida rica.</em></p>" +
+                    "<p class='transient'> La respuesta no ha convencido lo suficiente...</br></p>",
+                "opc3": "<br><p class='transient'><em>Yo - Es que tengo que....</em></p>" +
+                    "<p class='transient'><em>Pedro - ¡Venga ya, si te lo vas a pasar genial!.</em></p>" +
+                    "<p class='transient'> La respuesta no ha convencido lo suficiente...</br></p>"
+            }
+        }
+    ),
+    llamada3: new undum.SimpleSituation(
+        "<p></br> <em>Pedro - ¡Perfecto!. Te veo a la hora de cenar en la plaza donde se hará la hoguera. ¡Chao!</em> </p>" +
+        "<p class='transient'>Piensas '<em>Este Pedro es de lo que no hay. Debe de estar en peligro de extinción..</em>. </br></br> " +
+        "Ahora que te has comprometido con la ilusión de Pedro tienes que <a href='salon'>prepararte para salir</a>.</p>"
+        , {}
+    ),
+
+    salon: new undum.SimpleSituation(
+        "<h1>Sala de estar</h1>"
+        , {
+            enter: function (character, system, from) {
+                system.doLink('salon_opcs');
+            },
+        }
+    ),
+
+    salon_opcs: new undum.SimpleSituation(
+        "<p>Te encuentras en tu sala de estar...</br></br>\
+        Puedes <a href='./sala_tele' class='once sticky'>sentarte en el sofá y mirar la televisión.</a></br>\
+        Puedes <a href='./sala_fotos' class='once sticky'>mirar las fotos de tus padres junto a ti de pequeño.</a></br>\
+        Puedes <a href='sala_cuadro' class='once sticky'>mirar el cuadro que hay colgado en la pared.</a></br>\
+        O bien puedes <a href='moverse_casa'class='sticky'>ir a otra habitación</a></p>", {
+            actions: {
+                "sala_tele":  "<p>Te sientas y tal, muy cómodo todo pero ¡es San Juan y has quedado!. Te levantas porque tienes que salir.</p>",
+                "sala_fotos": "<p>Te acuerdas de tus padres cuando ibaís de vacaciones en verano. Qué buenos son.</p>",
+            }
+        }
+    ),
+
+
+    sala_cuadro: new undum.SimpleSituation(
+        "<p class='transient'>Te fijas en el cuadro. Hay gente alrededor de una hoguera en una zona pequeña del cuadro " +
+        "mientras que el resto del cuadro muestra un paisaje al amanecer. Recuerdas que el cuadro venía con " +
+        "la casa y que lo pintó el anterior dueño.</br> Piensas...</p>" +
+        "<p class='transient'><a href='moverse_casa' class='once'>Que cuadro más raro, tengo que cambiarlo algún día</a></p>" +
+        "<p class='transient'><a href='./opc2' class='once'>No sé que tiene, pero la verdad es que me gusta</a></p>"
+        , {
+            actions: {
+                "opc2": function (character, system, action) {
+                    if (character.qualities.secretos >= 1) {
+                        system.setQuality("secretos", character.qualities.secretos + 1);
+                    } else {
+                        system.setQuality("secretos", 1);
+                    }
+                }
+            }
+        }
+    ),
+
+    dormitorio_casa: new undum.SimpleSituation(
+        "<p>Te encuentras en tu dormitorio...</br></br>\
+        Puedes <a href='./dorm_cama' class='once sticky'>Hacer la cama.</a></br>\
+        Puedes <a href='dorm_siesta' class='once sticky'>Echar una siesta.</a></br>\
+        Puedes <a href='./dorm_pein' class='once sticky'>Vas al baño del dormitorio para peinarte.</a></br>\
+        O bien puedes <a href='moverse_casa'class='sticky'>volver a la Sala de Estar</a></p>"
+        , {
+            actions: {
+                'dorm_cama': "<p>No es el momento adecuado, pero aún así lo haces. Es mejor tenerla hecha.</br></p>"
+                ,
+                'dorm_pein': "<p>Sabes que eres guapo porque te lo dice tu madre, pero siempre se puede mejorar.</br></p>"
+            },
+            tags: ["hogar"],
+            optionText: "Ir al dormitorio",
+            displayOrder: 1
+        }
+    ),
+
+    dorm_siesta: new undum.SimpleSituation(
+        "<p>Crees que estas cansado (como siempre) y te hechas un rato, pero resulta que pones una mala postura y te duele el hombro." +
+        " Suficiente dormitorio por hoy (sales del dormitorio).</br></p>"
+        , {
+            enter: function (character, system, from) {
+                system.setQuality('suerte', character.qualities.suerte - 1);
+                system.doLink('moverse_casa');
+            },
+        }
+    ),
+
+    cocina_casa: new undum.SimpleSituation(
+        "<p>Te encuentras en tu cocina...</br></br>\
+       Puedes <a href='./notas' class='once sticky'>mirar las notas puestas del frigorífico.</a></br>\
+       Puedes <a href='./encimera' class='once sticky'>mirar la encimera de la cocina.</a></br>\
+       O bien puedes <a href='./mov'class='sticky'>volver a la Sala de Estar</a></p>"
+        , {
+            actions: {
+                'notas': "<p>Hay una lista: Manzanas, Pan, Arroz y algo que ni siquiera sabes qué pone.</br></p>",
+                'encimera': "<p>La limpiaste ayer, aunque siempre está la maldita mancha esa que no se va con nada.</br></p>",
+                "mov": function (character, system, action) {
+                    system.doLink('moverse_casa');
+                },
+            },
+            tags: ["hogar"],
+            optionText: "Ir a la cocina",
+            displayOrder: 2
+        }
+    ),
+
+    pasillo_casa: new undum.SimpleSituation(
+        "<p>Te encuentras en el pasillo de tu casa...</br></br>\
+       Puedes <a href='./perchero' class='once sticky'>mirar el perchero.</a></br>\
+       Puedes <a href='salir_casa' class='once'>salir de tu casa.</a></br>\
+       O bien puedes <a href='./mov'class='sticky'>volver a la Sala de Estar.</a></p>"
+        , {
+            actions: {
+                'perchero': "<p>Observas mejor y se aprecian varias cosas que deberías replantearte " +
+                    "guardar mañana u otro día como ese abrigo de piel (aunque seas más de invierno).</br></p>",
+                "mov": function (character, system, action) {
+                    system.doLink('moverse_casa');
+                },
+            },
+            tags: ["hogar"],
+            optionText: "Ir al pasillo",
+            displayOrder: 3
+        }
+    ),
+
+    salir_casa: new undum.SimpleSituation(
+        "<p class='transient'><br>Decides salir de tu hogar pero justo antes de salir te palpas los bolsillos. " +
+        "<em>¡Será posible!</em> Maldices mientras ya no notas las llaves por ningún lado...<br>" +
+        "<em><a href='fin'>¿Miro en el bolsillos de los pantalones?</a></em></br>" +
+        "<em><a href='fin'>¿Miro en el cerrojo de la puerta?</a></em><br>" +
+        "<em><a href='fin'>¿Y si salgo sin llaves?</a></em><br>" +
+        "</p>"
+        ,
+    ),
+
+
+    <!-- SALIMOS DE CASA -->
+
+    plantilla: new undum.SimpleSituation(
+        "<p class='transient'></br> texto </p>" +
+        "<p class='transient'><em><a href=''>X</a></em></p>" +
+        "<p class='transient'><em><a href=''>X</a></em></p>" +
+        "<p class='transient'><em><a href=''>X</a></em></p>"
+        , {}
+    ),
+
+    funcionalidades: new undum.SimpleSituation(
+        "<h1>FUNCIONALIDADES</h1>" +
         "<p><a href='fin'>FINALIZAR LA PARTIDA</a></p>"
         + "<br><p><a href='./con-moneda'>CON MONEDA</a></p>"
         + "<br><p><a href='./sin-moneda'>SIN MONEDA</a></p>"
@@ -36,34 +216,34 @@ undum.game.situations = {
         + "<br><p><a href='./con-amuleto'>CON AMULETO</a></p>"
         + "<br><p><a href='./sin-amuleto'>SIN AMULETO</a></p>"
         + "<br><p><a href='./fin-alt'>FIN ALT MOSTRAR</a></p>"
-        + "<br><p><a href='./fin-alt-sin'>FIN ALT SIN MOSTRAR</a></p>",{
+        + "<br><p><a href='./fin-alt-sin'>FIN ALT SIN MOSTRAR</a></p>", {
             actions: {
                 "sin-amuleto": function (character, system, action) {
-                    system.setQuality("amuleto",0);
+                    system.setQuality("amuleto", 0);
                 },
                 "con-amuleto": function (character, system, action) {
-                    system.setQuality("amuleto",1);
+                    system.setQuality("amuleto", 1);
                 },
                 "sin-moneda": function (character, system, action) {
-                    system.setQuality("moneda_antigua",0);
+                    system.setQuality("moneda_antigua", 0);
                 },
                 "con-moneda": function (character, system, action) {
-                    system.setQuality("moneda_antigua",1);
+                    system.setQuality("moneda_antigua", 1);
                 },
                 "sin-nota": function (character, system, action) {
-                    system.setQuality("nota",0);
+                    system.setQuality("nota", 0);
                 },
                 "con-nota": function (character, system, action) {
-                    system.setQuality("nota",1);
+                    system.setQuality("nota", 1);
                 },
-                "fin-alt": function (character, system, action){
+                "fin-alt": function (character, system, action) {
                     if (character.qualities.secretos >= 1) {
-                        system.setQuality("secretos", character.qualities.secretos+1);
+                        system.setQuality("secretos", character.qualities.secretos + 1);
                     } else {
                         system.setQuality("secretos", 1);
                     }
                 },
-                "fin-alt-sin": function (character, system, action){
+                "fin-alt-sin": function (character, system, action) {
                     character.qualities.secretos++;
                 }
             }
@@ -99,7 +279,7 @@ undum.game.qualities = {
     ),
     secretos: new undum.IntegerQuality(
         "<span title='Según tus respuestas en las conversaciones se podrán revelar otros detalles de la historia.'>Diálogos secretos</span>",
-        {priority:"0002", group: 'estadisticas'})
+        {priority: "0002", group: 'estadisticas'})
 };
 
 // ---------------------------------------------------------------------------
